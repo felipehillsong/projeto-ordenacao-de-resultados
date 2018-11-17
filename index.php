316b33da2534cd7be8e1c350490a96fa0bb439b4
Felipe Silva <feliperochasilva1987@gmail.com>
2018-11-17 03:35:02 -0200

    add project

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+try{
+    $pdo = new PDO("mysql:dbname=projeto_ordenar;host=localhost", "root", "");
+}catch(PDOException $e){
+    echo "Erro: ".$e->getMessage;
+}
+if(isset($_GET['ordem']) && !empty($_GET['ordem'])){
+    $ordem = addslashes($_GET['ordem']);
+    $sql = "SELECT * FROM usuarios ORDER BY ".$ordem;
+}else{
+    $ordem = "";
+    $sql = "SELECT * FROM usuarios";
+}
+?>
+<form method="GET">
+    <select name="ordem" onchange="this.form.submit()">
+    <option></option>
+    <option value="nome" <?php echo ($ordem == "nome")?'selected="seleceted"':''?>>Pelo Nome</option>
+    <option value="idade" <?php echo ($ordem == "idade")?'selected="seleceted"':''?>>Pela Idade</option>
+    </select>
+</form>
+<table border="1" width="400">
+    <tr>
+        <th>Nome</th>
+        <th>Idade</th>
+    </tr>
+<?php
+
+$sql = $pdo->query($sql);
+if($sql->rowCount() > 0){
+    foreach($sql->fetchAll() as $usuario):
+        ?>
+        <tr>
+            <td><?php echo $usuario['nome']; ?></td>
+            <td><?php echo $usuario['idade']; ?></td>
+        </tr>
+        <?php
+        endforeach;
+}
+?>
+ </table>
